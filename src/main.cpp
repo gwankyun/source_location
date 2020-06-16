43cd7654bde5434e4e3c1673aa65626644f0856d
權少 <ljqican@163.com>
2020-06-16 21:27:59 +0800

    首次提交

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "source_location/source_location.hpp"
+
+int main()
+{
+    auto&& loc = SOURCE_LOCATION_CURRENT();
+    std::cout << loc.file_name() << std::endl;
+    std::cout << loc.function_name() << std::endl;
+    std::cout << loc.line() << std::endl;
+    return 0;
+}
